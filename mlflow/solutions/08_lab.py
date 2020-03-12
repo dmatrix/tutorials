155cf406355ed2ca4c30233c23f832174a9a546e
Jules Damji <dmatrix@comcast.net>
2020-03-12 14:21:28 -0700

    Augmented README.md; Added a get started lab; refactored more code

--- mlflow/solutions/08_lab.py
+++ mlflow/solutions/08_lab.py
@@ -5,8 +5,9 @@ Read documentation on https://mlflow.org/docs/latest/python_api/mlflow.html#mlfl
 if __name__ == '__main__':
     params_1 = {'alpha': 0.5,
               'l1_ratio': 0.01}
-    params_2 = {'epochs': 5}
 
-    # Two runs from GitHub Project
+    # Run a project from GitHub Project
     mlflow.run("git://github.com/mlflow/mlflow-example.git", use_conda=False, parameters=params_1)
-    mlflow.run("git://github.com/dmatrix/mlflow-example.git", use_conda=False, parameters=params_2)
+    # TODO
+    # Add your GitHub MLflow Project here
+    # mlflow.run(<YOUR_PROJECT_GIT_URI>, args...,args)
